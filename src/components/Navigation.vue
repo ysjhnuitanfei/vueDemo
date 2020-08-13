25f94ebde84b4182b5ffd73a876ea253b98816e9
TF <tanfei@tanfeideMacBook-Pro.local>
2020-08-13 14:07:13 +0800

    初始化项目

---
+++ src/components/Navigation.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="nav-left"></div>
+</template>
+
+<script>
+export default {
+  name: 'navigation',
+  props: [''],
+  components: {},
+  data () {
+    return {
+
+    }
+  },
+  created () {
+
+  },
+  mounted () {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="less">
+</style>
